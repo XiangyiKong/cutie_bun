3724e8da9f3862970d25355677e620d338e87ee9
XiangyiKong <barbara.kong16@utexas.edu>
2015-03-19 23:56:32 -0500

    Update SocialNet.js

--- public/js/SocialNet.js
+++ public/js/SocialNet.js
@@ -1,3 +1,10 @@
 // Define the paths to all of the dependencies
 // Initialize and launch the user interface
-define
+define(['views/index'], function(indexView) {
+  var initialize = function() {
+    indexView.render();
+  }
+  return {
+    initialize: initialize
+  };
+});
