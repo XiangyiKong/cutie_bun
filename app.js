8da97d9c1c4553e9b722833395efc8921e474da8
XiangyiKong <barbara.kong16@utexas.edu>
2015-03-28 22:47:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 //handles routing for incoming HTTP requests
 var express = require('express');
 var app = express();
+var nodemailer = require('nodemailer');
+var MemoryStore = require('connect').session.MemoryStore;
+
+// Import the data layer
+var mongoose = require("mongoose");
+var config = {
+  mail: require('./config/mail')
+};
+
+// Import the accounts
+var Account = require('./models/Account')(config, mongoose, nodemailer);
 
 app.configure(function() {
   app.set('view engine', 'jade');
@@ -12,3 +23,5 @@ app.get('/', function(req, res)) {
 });
 
 app.listen(8080);
+
+// TODO: P73-P74
