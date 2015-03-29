d127cdc598957faf47bb220141059a1bd5d091f3
XiangyiKong <barbara.kong16@utexas.edu>
2015-03-28 21:08:44 -0500

    Update SocialNet.js
    Add login check
    Anyone versed in response codes will immediately understand 401 to mean the server responded properly but user authorization was either not provided or failed its security tests.

--- public/js/SocialNet.js
+++ public/js/SocialNet.js
@@ -8,3 +8,24 @@ define(['views/index'], function(indexView) {
     initialize: initialize
   };
 });
+
+define(['router'], function(router)) {
+  var initialize = function() {
+    // Responsible for booting up the router, which will control what appears in the web browser
+    checkLogin(runApplication);
+  };
+  
+  var checkLogin = function(callback) {
+    // AJAX request to the Express server
+    // If the user is allowed to access the deeper content within the application
+    $.ajax("/account/authenticated", {
+      method: "GET",
+      success: function() {
+        return callback(true);
+      },
+      error: function(data) {
+        return callback(false);
+      }
+    });
+  }
+};
